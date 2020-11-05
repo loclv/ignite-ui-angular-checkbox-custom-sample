a51df266d77b6fa676210fa7e0b33eb18956db7a
loclv <loclv@kaopiz.com>
2020-11-05 14:39:17 +0700

    add custom checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,22 @@
-import { BrowserModule, HammerModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule, HammerModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IgxCheckboxModule } from 'igniteui-angular';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomeComponent } from './home/home.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HomeComponent
-  ],
-  imports: [
-    BrowserModule,
-    HammerModule,
-    AppRoutingModule,
-    BrowserAnimationsModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [AppComponent, HomeComponent],
+    imports: [
+        BrowserModule,
+        HammerModule,
+        AppRoutingModule,
+        BrowserAnimationsModule,
+        IgxCheckboxModule,
+    ],
+    providers: [],
+    bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
